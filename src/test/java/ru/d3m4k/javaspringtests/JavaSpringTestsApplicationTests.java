4352efe8c9dc41cc2ecc610b59ae2cc895f3ecd1
DmitryErmakoff <dimka6466erm@gmail.com>
2024-10-20 22:27:11 +0500

    Initial commit

---
+++ src/test/java/ru/d3m4k/javaspringtests/JavaSpringTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.d3m4k.javaspringtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
