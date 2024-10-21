1b4257e2fdd147fd0338ce26c0eec0d51b73dc14
DmitryErmakoff <dimka6466erm@gmail.com>
2024-10-21 14:08:08 +0500

    add 2 integration tests

--- src/test/java/ru/d3m4k/javaspringtests/JavaSpringTestsApplicationTests.java
+++ src/test/java/ru/d3m4k/javaspringtests/JavaSpringTestsApplicationTests.java
@@ -1,5 +1,8 @@
 package ru.d3m4k.javaspringtests;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,48 @@ import org.springframework.boot.test.context.SpringBootTest;
 class JavaSpringTestsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void shouldReturnHelloMessage() {
+		String expectedResult = "Hello, someone";
+
+		Response response = RestAssured
+				.get("https://playground.learnqa.ru/api/hello")
+				.andReturn();
+
+		String receivedResult = response.jsonPath().getString("answer");
+		assertThat(receivedResult).isEqualTo(expectedResult);
 	}
 
+	@Test
+	public void shouldAddPetSuccessfully() {
+		String jsonBody = "{\n" +
+				"  \"id\": 0,\n" +
+				"  \"category\": {\n" +
+				"    \"id\": 0,\n" +
+				"    \"name\": \"string\"\n" +
+				"  },\n" +
+				"  \"name\": \"doggie\",\n" +
+				"  \"photoUrls\": [\n" +
+				"    \"string\"\n" +
+				"  ],\n" +
+				"  \"tags\": [\n" +
+				"    {\n" +
+				"      \"id\": 0,\n" +
+				"      \"name\": \"string\"\n" +
+				"    }\n" +
+				"  ],\n" +
+				"  \"status\": \"available\"\n" +
+				"}";
+
+		Response response = RestAssured
+				.given()
+				.header("Content-Type", "application/json")
+				.body(jsonBody)
+				.when()
+				.post("https://petstore.swagger.io/v2/pet")
+				.andReturn();
+
+		response.prettyPrint();
+
+		assertThat(response.getStatusCode()).isEqualTo(200);
+	}
 }
